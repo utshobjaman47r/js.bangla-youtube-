c04d1347f36ce9096e4df194d0798fc37030c3e0
utshob <utshobjaman2012@gmail.com>
2025-11-27 08:44:19 +0600

    ES6

--- final/script.js
+++ final/script.js
@@ -2,8 +2,27 @@
 
 
 
-dddd ;
+// dddd ;
 console.log("i am batman");
+
+const bookings =[];
+const createBooking = function(flightNum,numPassengers = 1,price = 199){
+   //ES 5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+  const booking ={
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+
+
+}
+createBooking('LH123');
+
+
 /*
 ///////////////////////////////////////
 // Default Parameters
